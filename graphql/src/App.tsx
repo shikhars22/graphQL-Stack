0d9824819da83dca3c9fc87fb00a43100f3db94f
shikhars22 <shikhars22@gmail.com>
2022-12-22 02:40:36 +0530

    Lesson 62 done

--- graphql/src/App.tsx
+++ graphql/src/App.tsx
@@ -1,10 +1,46 @@
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+import { useEffect } from 'react';
+
+const GET_DATA = gql`
+	query GetLaunches {
+		launchesPast(limit: 10) {
+			mission_name
+			launch_date_local
+		}
+	}
+`;
+
+export type Launch = {
+	mission_name: string;
+	launch_date_local: string;
+	launch_site: {
+		site_name_long: string;
+	};
+};
 
 function App() {
-  return (
-    <div className="App">Hello Skg
-    </div>
-  );
+	const { loading, error, data } = useQuery(GET_DATA);
+
+	useEffect(() => {
+		console.log(loading, error, data);
+	});
+
+	return (
+		<div className='App'>
+			<h2>My first Apollo app 🚀</h2>
+			{data
+				? data.launchesPast.map((launch: Launch) => {
+						return (
+							<p>
+								{launch.mission_name}
+								{launch.launch_date_local}
+							</p>
+						);
+				  })
+				: null}
+		</div>
+	);
 }
 
 export default App;
--- graphql/src/index.tsx
+++ graphql/src/index.tsx
@@ -2,12 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	gql,
+} from '@apollo/client';
+
+let client = new ApolloClient({
+	uri: 'https://api.spacex.land/graphql/',
+	cache: new InMemoryCache(),
+});
+
+client
+	.query({
+		query: gql`
+			{
+				launchesPast(limit: 10) {
+					mission_name
+					launch_date_local
+					launch_site {
+						site_name_long
+					}
+				}
+			}
+		`,
+	})
+	.then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>
 );
